fa1201bb04cbd69dcc122cfbf7eec57447e0f4d5
Ben Orchard <thefirstmuffinman@gmail.com>
2023-07-06 09:36:44 +0100

    add WIP repo init script
    (does not work, really just tells you what to do)

---
+++ repo-init.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+# Run in repo root directory
+# Manual steps:
+#   1. create (empty) repo on GH
+#   2. initialize local repo
+#   3. add remote
+#   4. copy over code, readme
+
+# EXPECTATIONS: main branch
+part_init() {
+    git add rpn-calc/src
+    git add .gitignore
+    git commit -m "initial commit: add basic RPN calculator
+
+Parses string input and handles a handful of operators.
+Includes a couple of unit tests.
+"
+
+    git add rpn-calc/docs
+    git add README.md
+    git commit -m "add readme"
+    # MANUAL: git push -u origin main
+}
+
+# MANUAL: copy .github folder over
+part_ci_pr() {
+    git checkout -b ci-more-tests
+    git add .github/
+    git commit -m "add GitHub Actions workflow for running unit tests
+
+Triggers on every commit to main, and every PR sync.
+"
+
+    echo "
+
+def test_subtract():
+    commands = ['2.0', '1.0', '-']
+    assert main(commands) == 1.0\
+" >> rpn-calc/src/tests/test_calculate.py
+    git add rpn-calc/src/tests/test_calculate.py
+    git commit -m "add a subtraction unit test
+
+Test currently failing."
+
+    git push -u origin ci-more-tests
+}
+
+part_integer_mode() {
+    git checkout main
+    git checkout -b integer-mode
+    # TODO: make changes
+    git commit -m "calculator from using floats to integers
+
+Division is now floor division, where 3 // 2 == 1."
+    git push -u origin integer-mode
+}
